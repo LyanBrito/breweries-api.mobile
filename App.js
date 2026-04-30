324aa8030bebb4e2d28c0e9643fc78e8b12809a2
lyanbrito <lyanbrito613@gmail.com>
2026-04-29 21:54:31 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import {View, Text, StyleSheet} from "react-native";
+
+export default function App() {
+
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
