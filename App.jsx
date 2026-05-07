2bee52aaa3c79d73e0748eae9e5235bdfed6e34c
lyanbrito <lyanbrito613@gmail.com>
2026-05-06 22:53:03 -0300

    feat: added multipage to project

--- App.jsx
+++ App.jsx
@@ -1,54 +1,52 @@
 import React from "react";
-import { StatusBar } from "react-native";
-import styled from "styled-components/native";
-import CardsWrapper from "./src/components/CardsWrapper";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+
+// Screens
+import HomeScreen from "./src/screens/HomeScreen";
+import FavoritesScreen from "./src/screens/FavoritesScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-    return (
-        <Container>
-            <StatusBar barStyle="light-content" />
-            <Header>
-                <Title>Breweries</Title>
-                <Subtitle>around the world</Subtitle>
-            </Header>
+    const [favorites, setFavorites] = React.useState([]);
 
-            <Content>
-                <CardsWrapper />
-            </Content>
-        </Container>
+    const toggleFavorite = (brewery) => {
+        setFavorites(prev => {
+            const exists = prev.find(item => item.id === brewery.id);
+
+            if (exists) {
+                return prev.filter(item => item.id !== brewery.id);
+            } else {
+                return [...prev, brewery];
+            }
+        });
+    };
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown: false}} id="1">
+                {/*Não estou chamando component aqui pq tenho que chamar as propriedades deles dessa forma*/}
+                <Stack.Screen name="Home">
+                    {(props) => (
+                        <HomeScreen
+                            {...props}
+                            favorites={favorites}
+                            toggleFavorite={toggleFavorite}
+                        />
+                    )}
+                </Stack.Screen>
+
+                <Stack.Screen name="Favorites">
+                    {(props) => (
+                        <FavoritesScreen
+                            {...props}
+                            favorites={favorites}
+                            toggleFavorite={toggleFavorite}
+                        />
+                    )}
+                </Stack.Screen>
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
-
-const Container = styled.ScrollView`
-    flex: 1;
-    background-color: #efc9ad;
-`;
-
-const Header = styled.View`
-  padding: 60px 20px 30px;
-  background-color: #F2A900;
-  border-bottom-left-radius: 30px;
-  border-bottom-right-radius: 30px;
-  align-items: center;
-`;
-
-const Title = styled.Text`
-  font-size: 32px;
-  font-weight: 800;
-  color: #2D1B08;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-`;
-
-const Subtitle = styled.Text`
-  font-size: 16px;
-  font-weight: 400;
-  color: #5E3A11;
-  font-style: italic;
-  margin-top: -5px;
-`;
-
-const Content = styled.View`
-  padding: 20px;
-  margin-top: 10px;
-`;
